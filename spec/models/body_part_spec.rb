442a2e699650e0fc39d69d83bc70e5d7a0a49a89
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 00:00:23 +0300

    Test suite for BodyPart validation and relationship

--- spec/models/body_part_spec.rb
+++ spec/models/body_part_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BodyPart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Todo model has a 1:m relationship with the Item model
+  it { should belong_to(:measure) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:size) }
 end
