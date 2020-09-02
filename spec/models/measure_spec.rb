07bac2cc48bb6dac5f92f28b60166542c9f94130
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 00:00:44 +0300

    Test suite for Measure validation and relationship

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Measure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+
+  # Ensure todo list 1:1 rlship mapping
+  it { should have_one(:body_part).dependent(:destroy)}
 end
