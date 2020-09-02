1a32bceb2a1a80b97e42852c6d383044c41c5e11
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 00:01:06 +0300

    Test suite for User model validation and relationship

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should have_many(:measures).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+end
