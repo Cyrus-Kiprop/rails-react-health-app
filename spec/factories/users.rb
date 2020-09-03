8d21a54967111ddcb1436c1157236642abaa1809
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 20:27:32 +0300

    update user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :user do
       name { Faker::Name.name }
         email { Faker::Internet.email }
-        password { Faker::Internet.password }
+        password_digest { Faker::Internet.password }
     end
 end
