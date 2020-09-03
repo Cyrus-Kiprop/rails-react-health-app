fea58236530013817c86cc90ffd54345778a4af2
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 18:52:23 +0300

    disable autoload

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -50,6 +50,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include RequestSpecHelper, type: :request
 
   config.include FactoryBot::Syntax::Methods
 
