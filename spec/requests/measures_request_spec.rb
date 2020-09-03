22176dc6b16aa91fa8c130e5cf713954e638062b
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-03 18:09:50 +0300

    Test suite for GET '/measurements/:id

--- spec/requests/measures_request_spec.rb
+++ spec/requests/measures_request_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe "Measures", type: :request do
     end
   end
 
+  # Test suite for GET /measurements/:id
+  describe 'GET /measurements/:id' do
+    before { get "/measurements/#{measure_id}" }
+
+    context 'when the record exists' do
+      it 'returns the todo' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(measure_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:measure_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
 end
