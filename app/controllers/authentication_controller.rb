40d8902edc56d8858ddf595c7ac263d11c876670
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 13:08:09 +0300

    protect api endpoints

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,7 @@
 class AuthenticationController < ApplicationController
   # return auth token once user is authenticated
+  skip_before_action :authorize_request, only: :authenticate
+
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
   # POST /signup
   # return authenticated token upon signup
   def create
