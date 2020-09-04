afa39d7d16618882ee7b04a162100fdf10b4f5e3
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 15:09:30 +0300

    configure measurement routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :measures do
-    resources :bodyparts
+    resources :measurements
   end
 
   post 'auth/login', to: 'authentication#authenticate'
