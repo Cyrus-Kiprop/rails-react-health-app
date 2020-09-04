ca4f47a3eb4bcf73e81597a75e1c378a1d0c7a19
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 14:11:20 +0300

    add association and validation between the Measure and Measurements models

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -2,6 +2,7 @@ class Measure < ApplicationRecord
   belongs_to :user
 
   has_one :body_part, dependent: :destroy
+  has_many :measurements, dependent: :destroy
 
   validates_presence_of(:body_part_name)
 
