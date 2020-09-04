902e4bfcb8119a23b81b0bee81cc7af7acdf44d8
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 14:09:10 +0300

    Test suite for measurement/measure model association and validation

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Measure, type: :model do
   # Association Test
 
   # Ensure todo list 1:1 rlship mapping
+  it { should have_many(:measurements).dependent(:destroy) }
   it { should have_one(:body_part).dependent(:destroy)}
 end
--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:measure) }
+
+  it { should validate_presence_of(:size) }
 end
