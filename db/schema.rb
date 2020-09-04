2111b5a720b92a94937ed3daf56d2073282377b9
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 11:52:33 +0300

    update column body_part to body_part_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_204020) do
+ActiveRecord::Schema.define(version: 2020_09_03_225140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_204020) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "body_part_name"
     t.index ["user_id"], name: "index_measures_on_user_id"
   end
 
