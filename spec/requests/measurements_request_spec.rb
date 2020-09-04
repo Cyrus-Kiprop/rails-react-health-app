856ad07fbd6a87e1ea56d510c0f803c0c64fede5
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-04 14:44:25 +0300

    Test suite for GET '/measurement/measure_id/measurements/:id'

--- spec/requests/measurements_request_spec.rb
+++ spec/requests/measurements_request_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Measurements", type: :request do
   let(:user) { create(:user) }
-  let!(:measure) { create(:todo) }
+  let!(:measure) { create(:measure) }
   let!(:measurement) { create_list(:measurement, 20, measure_id: measure.id) }
   let(:measure_id) { measure.id }
   let(:id) { measurement.first.id }
   let(:headers) { valid_headers }
 
-  # Test suite for GET /measures/:measure_id/items
-  describe 'GET /measures/:measure_id/items' do
-    before { get "/measures/#{measure_id}/items", params: {}, headers: headers }
+  # Test suite for GET /measures/:measure_id/measurements
+  describe 'GET /measures/:measure_id/measurements' do
+    before { get "/measures/#{measure_id}/measurements", params: {}, headers: headers }
 
     context 'when measure exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns all todo items' do
+      it 'returns all measure measurements' do
         expect(json.size).to eq(20)
       end
     end
@@ -30,7 +30,34 @@ RSpec.describe "Measurements", type: :request do
       end
 
       it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Todo/)
+        expect(response.body).to match(/Couldn't find measure/)
+      end
+    end
+  end
+
+  # Test suite for GET /measures/:measure_id/measurements/:id
+  describe 'GET /measures/:measure_id/measurements/:id' do
+    before { get "/measures/#{measure_id}/measurements/#{id}" }
+
+    context 'when measure measurement exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the measurement' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when measure measurement does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Measure/)
       end
     end
   end
