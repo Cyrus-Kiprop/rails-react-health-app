ab33206c4c9919037c5167683f959f7989d021a5
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-05 14:15:37 +0300

    Fix heroku isseus with secret key

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,7 +1,7 @@
 # app/lib/json_web_token.rb
 class JsonWebToken
   # secret to encode and decode token
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = 'somesecretkeyissuestakingplacehere' 
 
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
